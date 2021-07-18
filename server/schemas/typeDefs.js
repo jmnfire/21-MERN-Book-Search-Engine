3ca4f40b5cfebee64bcba768f95c2b0143819812
Jovan <jovan.m.nixon@gmail.com>
2021-07-18 15:14:05 -0600

    update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!), password: String!): Auth
-    saveBook(input: saveBook!): User
+    saveBook(bookData: BookInput!): User
     removeBook(bookId: ID!): User
   }
 
